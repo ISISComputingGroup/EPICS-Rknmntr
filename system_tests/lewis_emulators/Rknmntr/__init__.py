f662b316a58281911a91508a11c7e30d9a9d9d97
Jack Harper <jack.harper@stfc.ac.uk>
2024-08-06 08:33:57 +0100

    run safe ruff fixes

--- system_tests/lewis_emulators/Rknmntr/__init__.py
+++ system_tests/lewis_emulators/Rknmntr/__init__.py
@@ -1,5 +1,5 @@
-from .device import SimulatedRknmntr
 from ..lewis_versions import LEWIS_LATEST
+from .device import SimulatedRknmntr
 
 framework_version = LEWIS_LATEST
 __all__ = ['SimulatedRknmntr']
--- system_tests/tests/rknmntr.py
+++ system_tests/tests/rknmntr.py
@@ -1,12 +1,11 @@
+import random
 import unittest
 
+from parameterized import parameterized
 from utils.channel_access import ChannelAccess
 from utils.ioc_launcher import get_default_ioc_dir
 from utils.test_modes import TestModes
 from utils.testing import get_running_lewis_and_ioc, parameterized_list
-
-from parameterized import parameterized
-import random
 
 DEVICE_PREFIX = "RKNMNTR_01"
 
